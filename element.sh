409fa9a1281559376e553afbbf7fbcf68bda3edc
waskylinks <likitajoel@gmail.com>
2025-09-29 08:31:20 -0400

    feat: support symbol and name lookups

--- element.sh
+++ element.sh
@@ -15,3 +15,12 @@ then
     JOIN types USING(type_id)
     WHERE atomic_number=$1;")
 fi
+# Handle name and symbol lookup
+if [[ -z $RESULT ]]
+then
+  RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius
+    FROM elements
+    JOIN properties USING(atomic_number)
+    JOIN types USING(type_id)
+    WHERE symbol='$1' OR name='$1';")
+fi
