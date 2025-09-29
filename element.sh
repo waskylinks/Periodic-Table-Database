39e2afe280329ed7babf2f7929de7b81dc044b17
waskylinks <likitajoel@gmail.com>
2025-09-29 08:33:05 -0400

    fix: handle not found elements properly

--- element.sh
+++ element.sh
@@ -24,3 +24,11 @@ then
     JOIN types USING(type_id)
     WHERE symbol='$1' OR name='$1';")
 fi
+#Handle output + not found
+if [[ -z $RESULT ]]
+then
+  echo "I could not find that element in the database."
+else
+  IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELT BOIL <<< "$RESULT"
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+fi
