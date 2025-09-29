038549908d1ba1f6a441f9df674bdec1dc78ff26
waskylinks <likitajoel@gmail.com>
2025-09-29 08:28:52 -0400

    feat: add database query for atomic_number

--- element.sh
+++ element.sh
@@ -6,3 +6,12 @@ then
   echo "Please provide an element as an argument."
   exit
 fi
+# Handle atomic number look up
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius
+    FROM elements
+    JOIN properties USING(atomic_number)
+    JOIN types USING(type_id)
+    WHERE atomic_number=$1;")
+fi
